0aac33aefccf1eb6aca038b5ef1534d023bda3d4
Buney <dpkinser93@gmail.com>
2021-08-05 19:44:40 -0600

    seed func defined

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,11 @@
 const sequelize = require('../config/connection');
+const seedPosts = require('./postSeeds');
+const seedComments = require('./commentSeeds');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
-
-
+    await seedPosts();
+    await seedComments();
 }
 
 seedAll();
