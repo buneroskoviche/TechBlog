ed1ae994a502eb67ec1c7c93bd681928befda6bc
Buney <dpkinser93@gmail.com>
2021-08-07 14:34:46 -0600

    dashboard route and page added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,4 +28,38 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/dashboard', async (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
+  try {
+    const postData = await Post.findAll({
+      where: { user_id: req.session.user_id},
+      include: [{model: User}],
+      attributes: { exclude: ['password'] } 
+    });
+  
+    let posts;
+    // Check to see if any posts were found
+    if(postData.length !== 0) {
+      if(postData.length > 1) {
+        posts = postData.map(post => post.get({ plain: true }))
+      } 
+      if(postData.length === 1) {
+        posts = postData.get({ plain: true });
+      }
+    }
+  
+    res.render('dashboard', {
+      posts,
+      loggedIn: req.session.logged_in,
+    });
+
+  } catch (e) {
+    res.status(500).json(e);
+  }
+  
+});
 module.exports = router;
